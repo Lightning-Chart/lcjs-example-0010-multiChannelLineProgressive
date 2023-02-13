13e471e0476083e04e0f9786da2d105369aec2ca
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:46:39 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Multi-channel real-time data monitoring JavaScript Chart
+# Real-time data monitoring JavaScript Chart
 
-![Multi-channel real-time data monitoring JavaScript Chart](multiChannelLineProgressive.png)
+![Real-time data monitoring JavaScript Chart](multiChannelLineProgressive-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,24 +8,24 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
-*Multi-channel real-time data monitoring Line Chart*
+_Multi-channel real-time data monitoring Line Chart_
 
 Lightning-fast Line Chart visualization over multiple channels that progress on the same X Axis
 
 Widely used in all kinds of fields for monitoring live data from many (hundreds or even thousands) of data sources at the same time.
 
-Frames rendered per second (*FPS*) is recorded live, and displayed on the Chart title. FPS of 40-60 indicates a smooth running performance.
+Frames rendered per second (_FPS_) is recorded live, and displayed on the Chart title. FPS of 40-60 indicates a smooth running performance.
 
 ## Automatic data cleaning
 
@@ -33,12 +33,13 @@ In infinitely scrolling applications, cleaning old out of view data is extremely
 in this example `LineSeries.setMaxPointsCount` method is used to enable automatic data cleaning.
 For reference, see also `LineSeries.setDataCleaningThreshold`.
 
-The `setMaxPointsCount` method sets the *amount of data points*, that will always be retained in the *series head* (latest data).
-The full conditions for valid *data cleaning* are:
-1. There is a considerably large chunk of data out of view (visible data is not cleaned!).
-2. If the chunk is removed, the amount of data that remains is still more than *max points count*.
+The `setMaxPointsCount` method sets the _amount of data points_, that will always be retained in the _series head_ (latest data).
+The full conditions for valid _data cleaning_ are:
 
-In practice, this results in an application where you can even scroll back for some distance and see older data, but if you scroll far enough, you will find that the old data has been *cleaned*. This allows the application to run *forever*!
+1. There is a considerably large chunk of data out of view (visible data is not cleaned!).
+2. If the chunk is removed, the amount of data that remains is still more than _max points count_.
+
+In practice, this results in an application where you can even scroll back for some distance and see older data, but if you scroll far enough, you will find that the old data has been _cleaned_. This allows the application to run _forever_!
 
 
 ## API Links
@@ -53,25 +54,25 @@ In practice, this results in an application where you can even scroll back for s
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Line series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lineseries.html
-[Data patterns]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/datapattern.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Line series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LineSeries.html
+[Data patterns]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/interfaces/DataPattern.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
 
--- src/index.js
+++ src/index.js
@@ -4,20 +4,15 @@
 
 // Import LightningChartJS
 const lcjs = require('@arction/lcjs')
-// Import data Generator 
-const { createProgressiveFunctionGenerator } = require('@arction/xydata')
+
+// Import xydata
+const xydata = require('@arction/xydata')
+
+// Import data Generator
+const { createProgressiveFunctionGenerator } = xydata
 
 // Extract required parts from LightningChartJS.
-const {
-    lightningChart,
-    SolidFill,
-    SolidLine,
-    AxisScrollStrategies,
-    AxisTickStrategies,
-    ColorRGBA,
-    UIElementBuilders,
-    Themes,
-} = lcjs
+const { lightningChart, SolidFill, SolidLine, AxisScrollStrategies, AxisTickStrategies, ColorRGBA, UIElementBuilders, Themes } = lcjs
 
 const DATA_FREQUENCY_HZ = 1000
 const CHANNELS_AMOUNT = 10
@@ -26,44 +21,44 @@ const channelIntervalY = 2 // [-1, 1]
 
 const chart = lightningChart()
     .ChartXY({
-    // theme: Themes.darkGold
+        // theme: Themes.darkGold
     })
     .setTitle(`Multi-channel real-time monitoring (${CHANNELS_AMOUNT} chs, ${DATA_FREQUENCY_HZ} Hz)`)
 const axisX = chart
     .getDefaultAxisX()
-    .disableAnimations()
     .setScrollStrategy(AxisScrollStrategies.progressive)
-    .setInterval(-xIntervalMax, 0)
+    .setInterval({ start: -xIntervalMax, end: 0, stopAxisAfter: false })
     .setTitle('Data points per channel')
-const axisY = chart.getDefaultAxisY()
+const axisY = chart
+    .getDefaultAxisY()
     .setTickStrategy(AxisTickStrategies.Empty)
     .setTitle('< Channels >')
-    .disableAnimations()
     .setScrollStrategy(AxisScrollStrategies.expansion)
-    .setInterval(-channelIntervalY / 2, CHANNELS_AMOUNT * channelIntervalY)
+    .setInterval({ start: -channelIntervalY / 2, end: CHANNELS_AMOUNT * channelIntervalY, stopAxisAfter: false })
 
 const series = new Array(CHANNELS_AMOUNT).fill(0).map((_, iChannel) => {
     // Create line series optimized for regular progressive X data.
-    const nSeries = chart.addLineSeries({
+    const nSeries = chart
+        .addLineSeries({
             dataPattern: {
                 // pattern: 'ProgressiveX' => Each consecutive data point has increased X coordinate.
                 pattern: 'ProgressiveX',
                 // regularProgressiveStep: true => The X step between each consecutive data point is regular (for example, always `1.0`).
                 regularProgressiveStep: true,
-            }
+            },
         })
         .setName(`Channel ${iChannel + 1}`)
-        // Default color, but thickness = 1
-        .setStrokeStyle(style => style.setThickness(1))
+        // Use -1 thickness for best performance, especially on low end devices like mobile / laptops.
+        .setStrokeStyle((style) => style.setThickness(-1))
         .setMouseInteractions(false)
         // Enable automatic data cleaning.
-        .setMaxPointCount(xIntervalMax)
+        .setDataCleaning({ minDataPointCount: xIntervalMax })
 
     // Add custom tick for each channel.
     chart
         .getDefaultAxisY()
-        .addCustomTick(UIElementBuilders.AxisTick)
-        .setValue(( CHANNELS_AMOUNT - (1 + iChannel) ) * channelIntervalY)
+        .addCustomTick()
+        .setValue((CHANNELS_AMOUNT - (1 + iChannel)) * channelIntervalY)
         .setTextFormatter(() => `Channel ${iChannel + 1}`)
         .setGridStrokeStyle(
             new SolidLine({
@@ -76,23 +71,37 @@ const series = new Array(CHANNELS_AMOUNT).fill(0).map((_, iChannel) => {
 })
 
 // Add LegendBox.
-chart.addLegendBox().add(chart)
+chart
+    .addLegendBox()
+    .add(chart)
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-width',
-        maxWidth: 0.30,
+        maxWidth: 0.3,
     })
 
 // Define unique signals that will be used for channels.
 const signals = [
     { length: 100 * 2 * Math.PI, func: (x) => Math.sin(x / 100) },
     { length: 100 * 2 * Math.PI, func: (x) => Math.cos(x / 100) },
-    { length: 200 * 2 * Math.PI, func: (x) => Math.cos(x / 200) + Math.sin(x / 100) },
-    { length: 200 * 2 * Math.PI, func: (x) => Math.sin(x / 50) + Math.cos(x / 200) },
-    { length: 200 * 2 * Math.PI, func: (x) => Math.sin(x / 100) * Math.cos(x / 200) },
+    {
+        length: 200 * 2 * Math.PI,
+        func: (x) => Math.cos(x / 200) + Math.sin(x / 100),
+    },
+    {
+        length: 200 * 2 * Math.PI,
+        func: (x) => Math.sin(x / 50) + Math.cos(x / 200),
+    },
+    {
+        length: 200 * 2 * Math.PI,
+        func: (x) => Math.sin(x / 100) * Math.cos(x / 200),
+    },
     { length: 450 * 2 * Math.PI, func: (x) => Math.cos(x / 450) },
     { length: 800 * 2 * Math.PI, func: (x) => Math.sin(x / 800) },
-    { length: 650 * 2 * Math.PI, func: (x) => Math.sin(x / 200) * Math.cos(x / 650) },
+    {
+        length: 650 * 2 * Math.PI,
+        func: (x) => Math.sin(x / 200) * Math.cos(x / 650),
+    },
 ]
 
 // Generate data sets for each signal.
@@ -108,41 +117,36 @@ Promise.all(
             .then((data) => data.map((xy) => xy.y)),
     ),
 ).then((dataSets) => {
-    // Push more data in each frame, while keeping a consistent amount of incoming points according to specified stream rate as Hz.
-    let xPos = 0
-    let tPrev = performance.now()
-    let newDataModulus = 0
-    const streamMoreData = () => {
-        const tNow = performance.now()
-        const tDelta = tNow - tPrev
-        let newDataPointsCount = DATA_FREQUENCY_HZ * (tDelta / 1000) + newDataModulus
-        newDataModulus = newDataPointsCount % 1
-        newDataPointsCount = Math.floor(newDataPointsCount)
+    // Stream data into series.
+    let tStart = window.performance.now()
+    let pushedDataCount = 0
+    const xStep = 1000 / DATA_FREQUENCY_HZ
+    const streamData = () => {
+        const tNow = window.performance.now()
+        // NOTE: This code is for example purposes (streaming stable data rate without destroying browser when switching tabs etc.)
+        // In real use cases, data should be pushed in when it comes.
+        const shouldBeDataPointsCount = Math.floor((DATA_FREQUENCY_HZ * (tNow - tStart)) / 1000)
+        const newDataPointsCount = Math.min(shouldBeDataPointsCount - pushedDataCount, 1000) // Add max 1000 data points per frame into a series. This prevents massive performance spikes when switching tabs for long times
         const seriesNewDataPoints = []
         for (let iChannel = 0; iChannel < series.length; iChannel++) {
-            const nDataset = dataSets[iChannel % (dataSets.length - 1)]
+            const dataSet = dataSets[iChannel % dataSets.length]
             const newDataPoints = []
             for (let iDp = 0; iDp < newDataPointsCount; iDp++) {
-                const x = xPos + iDp
-                const iData = x % (nDataset.length - 1)
-                const ySignal = nDataset[iData]
-                const y = ((CHANNELS_AMOUNT - 1) - iChannel) * channelIntervalY + ySignal
+                const x = (pushedDataCount + iDp) * xStep
+                const iData = (pushedDataCount + iDp) % dataSet.length
+                const ySignal = dataSet[iData]
+                const y = (CHANNELS_AMOUNT - 1 - iChannel) * channelIntervalY + ySignal
                 const point = { x, y }
                 newDataPoints.push(point)
             }
             seriesNewDataPoints[iChannel] = newDataPoints
         }
-        series.forEach((nSeries, iSeries) => nSeries.add(seriesNewDataPoints[iSeries]))
-        xPos += newDataPointsCount
-
-        // Request next frame.
-        tPrev = tNow
-        requestAnimationFrame(streamMoreData)
+        series.forEach((series, iChannel) => series.add(seriesNewDataPoints[iChannel]))
+        pushedDataCount += newDataPointsCount
+        requestAnimationFrame(streamData)
     }
-    streamMoreData()
+    streamData()
 })
-
-
 
 // Measure FPS.
 let tStart = window.performance.now()
